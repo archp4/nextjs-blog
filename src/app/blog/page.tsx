cea8dd4f843f32878b9cca1c8e84c5d53d93ff6b
Arch Patel <archpatel4@gmail.com>
2023-07-22 14:36:19 +0530

    init

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+const BlogPage = () => {
+    return <h1>
+      Hello BlogBlog
+    </h1>
+  }
+  
+  export default BlogPage;
---
+++ src/app/page.tsx
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import Link from 'next/link';
+
+const getPostMetaData = () =>{
+  const folder = 'posts/';
+  const files =fs.readdirSync(folder);
+  const markdownPosts =files.filter((file) => file.endsWith('.md'));
+  const slugs =markdownPosts.map((file)=>file.replace(".md",''));
+  return slugs;
+};
+
+
+const HomePage = () => {
+  
+  const postMetaData = getPostMetaData();
+  const postPreviews = postMetaData.map((slug)=>(
+    <div>
+      <Link href={'/posts/'+slug}>
+        <h2>{slug}</h2>
+      </Link>
+      
+    </div>
+  ));
+  
+  return <div>{postPreviews}</div>
+}
+
+export default HomePage;
+
+
+
